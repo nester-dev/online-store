afd6e7f37d3fbc90a4c6e2dee52c6521edc59aea
nester-dev <nester-dev@gmail.com>
2022-07-18 00:20:25 +0300

    feat: implement searchForm cleaning by close icon

--- online-store/src/components/searchForm.ts
+++ online-store/src/components/searchForm.ts
@@ -4,11 +4,31 @@ import multipleFilter from './multipleFilter';
 
 export function searchFormEvent(data: SourceData, currentState: State) {
     const searchForm = document.querySelector('.search-form__input') as HTMLInputElement;
+    const searchClose = document.querySelector('.search-form__close') as HTMLSpanElement;
     searchForm.focus();
     searchForm.select();
 
+    searchClose.addEventListener('click', (event: Event) => {
+        const target = event.target as HTMLElement;
+
+        if (target.classList.contains('search-form__close-active')) {
+            searchClose.classList.remove('search-form__close-active');
+            currentState.searchTerm = '';
+            searchForm.value = '';
+            localStorage.setItem('state', JSON.stringify(currentState));
+
+            const filteredData = multipleFilter(data, currentState);
+            render(filteredData, currentState);
+        }
+    });
+
     searchForm.addEventListener('input', (event: Event) => {
         const value = (event.target as HTMLInputElement).value.trim();
+        if (value.length) {
+            searchClose.classList.add('search-form__close-active');
+        } else {
+            searchClose.classList.remove('search-form__close-active');
+        }
         currentState.searchTerm = value.toLowerCase();
         localStorage.setItem('state', JSON.stringify(currentState));
 
